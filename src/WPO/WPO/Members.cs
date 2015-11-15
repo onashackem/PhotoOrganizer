e6666308b4b081745bb31a7d81ff38e9e349fb65
Ondra Heřmánek <ondra.hermanek@gmail.com>
2015-11-15 21:48:37 +0100

    MB's album processed

--- src/WPO/WPO/Members.cs
+++ src/WPO/WPO/Members.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WPO
 {
@@ -10,37 +6,40 @@ namespace WPO
     public enum Members
     {
         MichalAMaja = 1 << 0,
-        HermanciSr = 1 << 1,
-        Hanka = 1 << 2,
+        Klarka = 1 << 1,
+        Babicky = 1 << 2,
         Flechtnerovi = 1 << 3,
         Rumburk = 1 << 4,
         Dvorakovi = 1 << 5,
         Buresovi = 1 << 6,
         PavlaARobert = 1 << 7,
-        Svedkyne = 1 << 8,
+        Druzicky = 1 << 8,
         Mews = 1 << 9,
-        Troupovi = 1 << 10,        
+        Stefkovi = 1 << 10,
         Ondrous = 1 << 11,
         Matty = 1 << 12,
         Kropackovi = 1 << 13,
-        Lucka = 1 << 14,
+        LuckaK = 1 << 14,
         Krizovi = 1 << 15,
         GymCk = 1 << 16,
         Anicka = 1 << 17,
         Kebrlovi = 1 << 18,
         MisaL = 1 << 19,
         Lena = 1 << 20,
-        Zuzca = 1 << 21,
-        Simca = 1 << 22,
-        Martinove = 1 << 23,
-        Stefkovi = 1 << 24,
+        Martinove = 1 << 22,
+        Krumlovaci = 1 << 21,
+        JanaF = 1 << 22,
+        Charlie = 1 << 23,
 
-        FB = 1 << 30,        
-        Verejne = 1 << 31,
+        Verejne = 1 << 30,
+        KTisku = 1 << 31,
+        My = 1 << 32,
 
-        HermankovoKridlo = HermanciSr | Kropackovi | Krizovi,
-        VeseleKridlo = Hanka | Rumburk,
-        All = MichalAMaja | HermanciSr | Hanka | Flechtnerovi | Rumburk | Dvorakovi | Buresovi | PavlaARobert | Svedkyne | Mews | Martinove |
-            Troupovi | Ondrous | Matty | Kropackovi | Lucka | Krizovi | GymCk | Anicka | Kebrlovi | MisaL | Lena | Zuzca | Simca | Stefkovi
+        HermankovoKridlo = Klarka | Kropackovi | Krizovi,
+        VeseleKridlo = Dvorakovi | Rumburk,
+        GymplAll = JanaF | GymCk,
+        MewsAll = Mews | Charlie,
+        All = MichalAMaja | Klarka | Flechtnerovi | Rumburk | Dvorakovi | Buresovi | PavlaARobert | Druzicky | Mews | Martinove | My |
+            Ondrous | Matty | Kropackovi | LuckaK | Krizovi | GymCk | Anicka | Kebrlovi | MisaL | Lena | Krumlovaci | Stefkovi | JanaF | Charlie
     }
 }
